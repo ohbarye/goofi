916ae195fbf5f8a8f140c6ab07a50dc71d813044
ohbarye <over.rye@gmail.com>
2018-07-07 02:19:06 +0900

    Set CSS Baseline
    https://material-ui.com/style/css-baseline/

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,18 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+function MyApp() {
+  return (
+    <>
+      <CssBaseline />
+      <App />
+    </>
+  );
+}
 
 ReactDOM.render(
-  <App />,
+  MyApp(),
   document.getElementById('root') as HTMLElement
 );
