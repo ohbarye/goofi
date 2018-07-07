97f26614617ef3083508ed94e0f1d9af928bc07a
ohbarye <over.rye@gmail.com>
2018-07-07 10:47:36 +0900

    Change theme color

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,17 +3,27 @@ import * as ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/blue';
+import green from '@material-ui/core/colors/orange';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green,
+  },
+});
 
 function MyApp() {
   return (
-    <>
+    <MuiThemeProvider theme={theme}>
       <CssBaseline />
       <App />
-    </>
+    </MuiThemeProvider>
   );
 }
 
 ReactDOM.render(
-  MyApp(),
+  <MyApp />,
   document.getElementById('root') as HTMLElement
 );
