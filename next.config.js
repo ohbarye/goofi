6253d4fa437e394ff121fd36388c2e19eb83c54c
ohbarye <over.rye@gmail.com>
2019-10-26 02:10:37 +0900

    Use env instead of publicRuntimeConfig
    since it's not available in now v2
    
    https://github.com/zeit/next.js/blob/master/errors/serverless-publicRuntimeConfig.md

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withOffline = require('next-offline')
 const isDev = process.env.NODE_ENV !== 'production'
 
 module.exports = withOffline({
-  publicRuntimeConfig: {
+  env: {
     api: isDev ? 'http://localhost:3000' : typeof window !== 'undefined' ? '' : process.env.NOW_URL,
     isDev,
   },
