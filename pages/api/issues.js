d5aace242990f7a14efda98fd8e88e95299ad211
ohbarye <over.rye@gmail.com>
2019-10-27 21:49:19 +0900

    Add issues endpoint

---
+++ pages/api/issues.js
@@ -0,0 +1,9 @@
+import { getGoodFirstIssues } from '../../server/graphql';
+
+export default async (req, res) => {
+  const { language, endCursor, perPage } = req.query;
+  const result = await getGoodFirstIssues({language, endCursor, perPage});
+
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.send(result);
+}
